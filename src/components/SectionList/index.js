fc31a3d59bf53f78a3fe6297f1c3423477fb594c
luiz-junior <junior.rodrigues0222@gmail.com>
2019-09-16 14:22:52 -0300

    added folders and configurations

---
+++ src/components/SectionList/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './styles';
+
+class SectionList extends Component {
+  render() {
+    return (
+        <div className="list-container">
+
+        </div>
+    )
+  }
+}
+
+export default SectionList;
